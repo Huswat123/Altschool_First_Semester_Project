cab17b28297869ce6bb87591af94324156c15c0e
Huswatlawal <129822567+Huswat123@users.noreply.github.com>
2023-12-24 23:35:38 +0100

    Update expenses.py

--- expenses.py
+++ expenses.py
@@ -1,9 +1,11 @@
-import uuid
+from uuid import uuid4 
 from datetime import datetime, timezone
 
 
 class Expense:
     def __init__(self, title, amount):
+""" A method that takes the title and amount as parameters and sets the id, title, amount, created_at and updated_at attributes.
+        """
         self.id = str(uuid.uuid4())
         self.title = title
         self.amount = amount
@@ -11,6 +13,8 @@ class Expense:
         self.updated_at = self.created_at
 
     def update(self, title=None, amount=None):
+""" A method that takes the title and amount as parameters and updates the title and amount attributes.If the update method is called without any parameters, the title and amount attributes remain unchanged. The updated_at attribute is updated to the current date and time in UTC.
+        """
         if title:
             self.title = title
         if amount:
@@ -18,6 +22,8 @@ class Expense:
         self.updated_at = datetime.utcnow()
 
     def to_dict(self):
+    """ A method that returns a dictionary of the set of values inputted after the update method is called.
+        """
         return {
             "id": self.id,
             "title": self.title,
@@ -29,24 +35,36 @@ class Expense:
 
 class ExpenseDatabase:
     def __init__(self):
+""" A method that initializes an empty list of expenses.
+        """
         self.expenses = []
 
     def add_expense(self, expense):
+""" A method that takes an expense as a parameter and adds it to the list of expenses.
+        """
         self.expenses.append(expense)
 
     def remove_expense(self, expense_id):
+ """ A method that takes an expense_id as a parameter and removes the expense with the given id from the list of expenses.
+        """
         self.expenses = [e for e in self.expenses if e.id != expense_id]
 
     def get_expense_by_id(self, expense_id):
+""" A method that takes an expense_id as a parameter and returns the expense with the given id.
+        """
         for expense in self.expenses:
             if expense.id == expense_id:
                 return expense
         return None
 
     def get_expenses_by_title(self, title):
+""" A method that takes an expense_title as a parameter and returns the expense with the given title.
+        """
         return [expense for expense in self.expenses if expense.title == title]
 
     def to_dict(self):
+""" A method that returns a copy of the list of expenses.
+        """
         return [expense.to_dict() for expense in self.expenses]
 
 
